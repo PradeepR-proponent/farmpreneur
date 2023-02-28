191472c3b7e1d63582890fa36e2c0fe8ea949e99
PradeepR-proponent <pradeep.r@proponenttechnologies.com>
2023-02-28 18:11:45 +0530

    appbar icon size changed

--- src/components/TrainingHeader/TrainingHeader.js
+++ src/components/TrainingHeader/TrainingHeader.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, View, } from "react-native";
-import { Appbar} from 'react-native-paper';
+import { Appbar } from 'react-native-paper';
 import { useSelector } from "react-redux";
 import { translate } from '../../languageFeature'
 
@@ -14,18 +14,19 @@ export default function Header(props) {
     const headerTitle = scene?.descriptor?.options?.headerTitle
     return (
         <View style={styles.headerView}>
-            {previous ? <Appbar.BackAction onPress={navigation.goBack} /> : (showMenu && <Appbar.Action icon="menu" onPress={() => navigation.toggleDrawer()} />)}
+            {previous ? <Appbar.BackAction size={20}  onPress={navigation.goBack} /> : (showMenu && <Appbar.Action icon="menu" onPress={() => navigation.toggleDrawer()} />)}
             <Appbar.Content title={headerTitle ? translate(appLanguage, `${headerTitle}`) : ""} />
         </View>
     )
 }
-
+    
 const styles = StyleSheet.create({
     badgeIconWrapper: {
         width: 40,
     },
     headerView: {
-        padding: 0
+        padding: 0,
+        
     },
     badgeIcon: {
         backgroundColor: "white",
